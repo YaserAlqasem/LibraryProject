97016685fbe4845a4caae7ae41e61721f188cb2e
Abdullah <aboodfoqha@outlook.com>
2022-03-02 16:54:11 -0600

    merge

--- src/ui/library/UI.java
+++ src/ui/library/UI.java
@@ -19,30 +19,30 @@ public class UI {
         LibraryMember lm = new LibraryMember("A", "a", "a", "a", new Address("a", "a", "a", "a"));
 
         a.saveNewMember(lm);
-//        while (true) {
-//            System.out.println("Please select one of the options");
-//            if (user == null) {
-//                System.out.println("1 - login");
-//            } else {
-//                System.out.println("2 - add new library member");
-//                System.out.println("3 - add new book");
-//                System.out.println("4 - check out book");
-//                System.out.println("5 - add book copy");
-//                System.out.println("6 - logout");
-//            }
-//            int option = in.nextInt();
-//            switch (option) {
-//                case 1 -> {
-//                    Util.print("User ID");
-//                    String userId = in.next();
-//                    Util.print("Password");
-//                    String userPassword = in.next();
-//                    //TODO Controller Call To get User (LOGIN)
-//                }
-//                case 2 -> {
-//                    System.out.println("Please Enter new Member First Name");
-//                }
-//            }
-//        }
+        while (true) {
+            System.out.println("Please select one of the options");
+            if (user == null) {
+                System.out.println("1 - login");
+            } else {
+                System.out.println("2 - add new library member");
+                System.out.println("3 - add new book");
+                System.out.println("4 - check out book");
+                System.out.println("5 - add book copy");
+                System.out.println("6 - logout");
+            }
+            int option = in.nextInt();
+            switch (option) {
+                case 1 -> {
+                    Util.print("User ID");
+                    String userId = in.next();
+                    Util.print("Password");
+                    String userPassword = in.next();
+                    //TODO Controller Call To get User (LOGIN)
+                }
+                case 2 -> {
+                    System.out.println("Please Enter new Member First Name");
+                }
+            }
+        }
     }
 }
